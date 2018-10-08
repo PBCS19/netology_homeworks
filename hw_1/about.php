350d274e6aba53745646277005e59e581f17fa02
Viktor Kostarev <kostarev.victor15@yandex.ru>
2018-10-08 16:21:19 +0500

    Initial commit.

---
+++ hw_1/about.php
@@ -0,0 +1,46 @@
+<?php
+    $userName = 'Виктор';
+    $userSurname = 'Костарев';
+    $useAge = 21;
+    $userEmail = 'kostarev.victor15@yandex.ru';
+    $userCity = 'Новоуральск';
+    $userAbout = 'Студент';
+?>
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <title>Домашнее задание №1</title>
+  </head>
+  <body>
+    <div>
+      <h2>Информация о пользователе <?= $userName . ' ' . $userSurname ?></h2>
+      <table border="1">
+        <tr>
+		  <td>Имя:</td>
+		  <td><?= $userName ?></td>
+		</tr>
+        <tr>
+		  <td>Фамилия:</td>
+		  <td><?= $userSurname ?></td>
+		</tr>  
+        <tr>
+		  <td>Возраст:</td>
+		  <td><?= $useAge ?></td>
+		</tr>
+        <tr>
+		  <td>Адрес электронной почты:</td>
+		  <td><a href="mailto:<?= $userEmail ?>"><?= $userEmail ?></td>
+		</tr>
+        <tr>
+		  <td>Город:</td>
+		  <td><?= $userCity ?></td>
+		</tr>
+        <tr>
+		  <td>О себе:</td>
+		  <td><?= $userAbout ?></td>
+		</tr>
+      </table>
+    </div>
+  </body>
+</html>
