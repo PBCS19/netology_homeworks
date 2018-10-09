cba77bb2deff7914c8fb7ccf5a8c37dfed94564e
Viktor Kostarev <kostarev.victor15@yandex.ru>
2018-10-09 14:30:21 +0500

    hw_1_dop

---
+++ hw_1_dop/index.php
@@ -0,0 +1,29 @@
+<?php
+$x = (int) $_POST['x'];
+$a = 1;
+$b = 1;
+while ($a <= $x) {
+    $c = $a;
+    $a = $a + $b;
+    $b = $c;
+}
+if (isset($_POST['x']) && $a > $x) {
+    echo "Задуманное число ($x) НЕ входит в числовой ряд ($a,$b,$c)";
+} elseif ($a == $x) {
+    echo "Задуманное число ($x) входит в числовой ряд($a,$b,$c)";
+}
+?>
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <title>Домашнее задание №1 (дополнительное)</title>
+  </head>
+  <body>
+    <form method="post" action="">
+      <label>Введите число:</label>
+      <input type="text" name="x">
+      <button type="submit">Отправить</button>
+    </form>
+  </body>
+</html>
